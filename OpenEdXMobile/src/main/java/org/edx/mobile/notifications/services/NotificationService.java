3854c2b90bb39cc59317b4bf063f63b3af44ec27
oronibrian <brianoroni6@gmail.com>
2020-08-06 19:51:37 +0300

    push notification

--- OpenEdXMobile/src/main/java/org/edx/mobile/notifications/services/NotificationService.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/notifications/services/NotificationService.java
@@ -1,5 +1,7 @@
 package org.edx.mobile.notifications.services;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -10,6 +12,7 @@ import org.edx.mobile.logger.Logger;
 
 public class NotificationService extends FirebaseMessagingService {
     protected static final Logger logger = new Logger(NotificationService.class.getName());
+    private static final String TAG = "FCM Service";
 
     @Override
     public void onNewToken(String s) {
@@ -24,5 +27,8 @@ public class NotificationService extends FirebaseMessagingService {
         if (environment.getConfig().areFirebasePushNotificationsEnabled()) {
             PushLinkManager.INSTANCE.onFCMForegroundNotificationReceived(remoteMessage);
         }
+
+        Log.e(TAG, "From: " + remoteMessage.getFrom());
+        Log.e(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
     }
 }
